266e22f2d3bdc1c12ca936ec426fbd78fd1047be
dedeepya-42 <11239a042@kanchiuniv.ac.in>
2026-03-30 15:20:37 +0530

    Add files via upload

---
+++ Black Hat Transformation/Black Hat Transformation.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("image.jpg", 0)
+
+# Prevent crash if the image is not loaded
+if img is None:
+    print("Error: Image not found")
+    exit()
+
+kernel = np.ones((9,9), np.uint8)
+blackhat = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, kernel)
+
+cv2.imshow("Original", img)
+cv2.imshow("Black Hat", blackhat)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
