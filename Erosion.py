cceaa11959c120330ccca97bbe22cf80a4f80bb0
dedeepya-42 <11239a042@kanchiuniv.ac.in>
2026-03-30 14:53:31 +0530

    Add files via upload

---
+++ Erosion.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("image.jpg", 0)
+
+# Check if image is loaded
+if img is None:
+    print("Error: Image not found")
+    exit()
+
+kernel = np.ones((5,5), np.uint8)
+erosion = cv2.erode(img, kernel, iterations=1)
+
+cv2.imshow("Original", img)
+cv2.imshow("Erosion", erosion)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
